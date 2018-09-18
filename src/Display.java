0ef2213428643c136fd9cd6fff90ed6f6086e345
Damon <ericke98@gmail.com>
2018-09-18 11:22:28 -0400

    swswswd

--- src/Display.java
+++ src/Display.java
@@ -1,9 +1,23 @@
-import java.awt.Component;
-
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Toolkit;
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Rectangle;
 
 public class Display {
 
     public static void main(String[] args) {
 	// write your code here
+    }
+
+    private class DisplayPanel extends JPanel {
+
+        public void paintComponent(Graphics g) {
+
+            
+
+        }
+
     }
 }
