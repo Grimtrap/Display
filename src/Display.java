a5c1de7b1645a999104214c0ff21f95090fcc602
Damon <ericke98@gmail.com>
2018-09-19 11:21:35 -0400

    actually displays something now

--- src/Display.java
+++ src/Display.java
@@ -5,20 +5,29 @@ import java.awt.Graphics;
 import java.awt.Color;
 import java.awt.Rectangle;
 
-public class Display {
+public class Display extends JFrame{
 
     public static void main(String[] args) {
-	// write your code here
+	    new Display(new Bracket());
     }
 
     public Display(Bracket tournament) {
 
 
+        super("Tournament Bracket");
+
+        JPanel displayPanel = new DisplayPanel();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(Toolkit.getDefaultToolkit().getScreenSize().width,Toolkit.getDefaultToolkit().getScreenSize().height);
+        this.requestFocusInWindow(); //make sure the frame has focus
+        this.setVisible(true);
     }
 
     private class DisplayPanel extends JPanel {
 
         public void paintComponent(Graphics g) {
+
+
 
             super.paintComponent(g);
             setDoubleBuffered(true);
