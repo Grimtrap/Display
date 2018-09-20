f12316bf526924def5dcb8e8d9e2d8b8cbc6d0f6
Damon <ericke98@gmail.com>
2018-09-20 11:05:15 -0400

    image

--- src/Display.java
+++ src/Display.java
@@ -1,40 +1,96 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.Toolkit;
-import java.awt.Graphics;
-import java.awt.Color;
-import java.awt.Rectangle;
+//Graphics &GUI imports
+import javax.swing.*;
+import java.awt.*;
 
-public class Display extends JFrame{
+//Keyboard imports
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+
+//Util
+
+
+class Display extends JFrame {
+
+    //class variables
+    static JFrame window;
+    JPanel displayPanel;
+
+
+    //Main
     public static void main(String[] args) {
-	    new Display(new Bracket());
+        window = new Display();
     }
 
-    public Display(Bracket tournament) {
+
+    //Constructor - this runs first
+    Display() {
+        super("My Game");
 
 
-        super("Tournament Bracket");
-
-        JPanel displayPanel = new DisplayPanel();
+        // Set the frame to full screen
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(Toolkit.getDefaultToolkit().getScreenSize().width,Toolkit.getDefaultToolkit().getScreenSize().height);
+        this.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+        //frame.setResizable(false);
+
+
+        //Set up the game panel (where we put our graphics)
+        displayPanel = new DisplayPanel();
+        this.add(new DisplayPanel());
+
+        MyKeyListener keyListener = new MyKeyListener();
+        this.addKeyListener(keyListener);
+
         this.requestFocusInWindow(); //make sure the frame has focus
+
         this.setVisible(true);
+
+
+    } //End of Constructor
+
+
+    /**
+     * --------- INNER CLASSES -------------
+     **/
+
+    // Inner class for the the game area - This is where all the drawing of the screen occurs
+    private class DisplayPanel extends JPanel {
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g); //required
+            setDoubleBuffered(true);
+            Image match = new ImageIcon("resources/match.png").getImage();
+            //move enemies
+
+
+            //check for collision
+
+            g.drawImage(match,500,500,500,250,null);
+            //repaint
+            repaint();
+        }
     }
 
-    private class DisplayPanel extends JPanel {
+    // -----------  Inner class for the keyboard listener - this detects key presses and runs the corresponding code
+    private class MyKeyListener implements KeyListener {
 
-        public void paintComponent(Graphics g) {
-
-
-
-            super.paintComponent(g);
-            setDoubleBuffered(true);
-
-
+        public void keyTyped(KeyEvent e) {
         }
 
-    }
+        public void keyPressed(KeyEvent e) {
+            //System.out.println("keyPressed="+KeyEvent.getKeyText(e.getKeyCode()));
 
+            if (KeyEvent.getKeyText(e.getKeyCode()).equals("A")) {  //If 'D' is pressed
+
+            } else if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {  //If ESC is pressed
+                System.out.println("Quitting!"); //close frame & quit
+                window.dispose();
+
+            }
+        }
+
+        public void keyReleased(KeyEvent e) {
+        }
+        //end of keyboard listener
+    }
 }
+  
